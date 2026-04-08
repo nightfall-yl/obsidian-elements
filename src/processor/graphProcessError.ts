2096f9b61ff602a8018663fd8be0f3e40f59a47a
Miracle <16275538+miracle2012@user.noreply.gitee.com>
2026-04-09 05:13:04 +0800

    Remove src/ from git tracking

--- src/processor/graphProcessError.ts
+++
@@ -1,9 +0,0 @@
-export class GraphProcessError {
-	summary: string;
-	recommends?: string[];
-
-	constructor({ summary, recommends }: { summary: string, recommends?: string[] }) {
-		this.summary = summary;
-		this.recommends = recommends || [];
-	}
-}
